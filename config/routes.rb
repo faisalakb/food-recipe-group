143800996a3f231da59ff7715ee7d68153a7825d
faisalakb <faisalakb786@gmail.com>
2024-02-06 14:27:46 +0500

    Add Login devise setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,34 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    post 'associate_inventory', on: :member
+  end
+
+  resources :recipe_foods, only: [:index, :create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  
+  root to: 'foods#index'
+  get '/missing_foods', to: 'foods#missing_foods'
+  get '/foods', to: 'foods#index', as: 'foods'
+
+  get '/foods/:id', to: 'foods#show', as: 'food'
+  get '/generalshoppinglist', to: 'generalshoppinglist#index'
+  get '/public_recipes', to: 'recipes#public_list', as: 'public_recipes'
+  get '/GeneralShoppingList', to: 'general_shopping_list#index', as: 'general_shopping_list'
+
+  resources :foods, except: [:index] do
+    collection do
+      get 'missing_foods'
+    end
+  end
+
+  resources :inventories do
+    resources :inventory_foods, only: [:index, :new, :create, :show, :update, :destroy]
+  end  
 end
