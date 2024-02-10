372ac2ee988163fe349231a63c1db5fc36a32826
faisalakb <faisalakb786@gmail.com>
2024-02-10 14:55:01 +0500

    Update inventories

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,35 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # Define resources for recipes
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    # Define a custom action for associating inventory with a recipe
     post 'associate_inventory', on: :member
+    # Define resources for recipe foods
+    resources :recipe_foods, only: [:index, :create]
   end
 
-  resources :recipe_foods, only: [:index, :create]
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  
-  root to: 'foods#index'
-  get '/missing_foods', to: 'foods#missing_foods'
-  get '/foods', to: 'foods#index', as: 'foods'
-
-  get '/foods/:id', to: 'foods#show', as: 'food'
-  get '/generalshoppinglist', to: 'generalshoppinglist#index'
-  get '/public_recipes', to: 'recipes#public_list', as: 'public_recipes'
-  get '/GeneralShoppingList', to: 'general_shopping_list#index', as: 'general_shopping_list'
-
+  # Define resources for foods
   resources :foods, except: [:index] do
     collection do
+      # Define a custom action for displaying missing foods
       get 'missing_foods'
     end
   end
 
+  # Define resources for inventories and nested resources for inventory foods
   resources :inventories do
     resources :inventory_foods, only: [:index, :new, :create, :show, :update, :destroy]
-  end  
+  end
+
+  # Define routes for health check
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Define the root path route
+  root to: 'foods#index'
+
+  # Define additional routes
+  get '/generalshoppinglist', to: 'generalshoppinglist#index'
+  get '/public_recipes', to: 'recipes#public_list', as: 'public_recipes'
+  get '/GeneralShoppingList', to: 'general_shopping_list#index', as: 'general_shopping_list'
 end
