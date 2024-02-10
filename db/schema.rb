05355b1712416b7957eb72a8cef3989ad46c0f97
faisalakb <faisalakb786@gmail.com>
2024-02-11 02:43:47 +0500

    Updated the reviwers comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_10_110506) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_10_213912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_10_110506) do
 
   create_table "inventories", force: :cascade do |t|
     t.string "name"
-    t.string "description"
     t.bigint "users_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
