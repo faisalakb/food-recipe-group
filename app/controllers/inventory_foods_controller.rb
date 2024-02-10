1d601d1f415168d53ea7e83576c2d4e5c6b11da6
faisalakb <faisalakb786@gmail.com>
2024-02-10 15:20:03 +0500

    Updated controllers

---
+++ app/controllers/inventory_foods_controller.rb
@@ -0,0 +1,17 @@
+class InventoryFoodsController < ApplicationController
+  before_action :set_inventory
+
+  def index
+    @inventory_foods = @inventory.inventory_foods
+  end
+
+  def new
+    @inventory_food = @inventory.inventory_foods.build
+  end
+
+  private
+
+  def set_inventory
+    @inventory = Inventory.find(params[:inventory_id])
+  end
+end
